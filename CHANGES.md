faa4baa5083815f22c0d02f1e2f139c6900d3a8a
svenkatreddy <svenkatreddy@users.noreply.github.com>
2021-05-05 02:16:10 +0000

    1. Update packages

--- CHANGES.md
+++ CHANGES.md
@@ -1,2 +1,5 @@
+## Next version
+- update pacakges.
+
 ## 1.0.6
 - update pacakges, publish.
--- test/basic.js
+++ test/basic.js
@@ -1,12 +1,18 @@
 const puppeteer = require('puppeteer');
 
 (async() => {
-
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
-await page.goto('http://example.com');
-await page.screenshot({path: 'example.png'});
-console.log("success");
-browser.close();
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox'],
+    });
+    const page = await browser.newPage();
+    await page.goto('http://example.com');
+    await page.screenshot({path: 'example.png'});
+    console.log("success");
+    browser.close();
+} catch(error) {
+  console.log(error);
+}
 
 })();
