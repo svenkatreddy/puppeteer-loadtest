ff4669ec1901ca51c8bd35b802c5808d2056dc39
Alain <alain.vandenberg@anywhere365.net>
2022-11-29 13:25:24 +0100

    Add ability to pass the concurrent id to the file under test
    Using --addId it will pass it as the first argument

--- test/index-test.js
+++ test/index-test.js
@@ -1,22 +1,50 @@
 
+const decache = require('decache');
 const expect = require('ultimate-chai').expect;
 const mock = require('mock-require');
 const sinon = require('sinon');
-const execStub = sinon.sandbox.create().stub();
-mock('child_process', { exec: execStub });
 
-process.argv.push("--file=./test/basic.js");
-process.env.DEBUG = 'puppeteer-loadtest'; 
-const index = require('../bin');
-
+let sandbox;
+let originalArgv = process.argv.slice();
 
 describe('index.js', () => {
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+  });
+  afterEach(() => {
+    // Decache so we can re-import:
+    decache('../bin');
+    // Restore the original process.argv:
+    process.argv = originalArgv.slice();
+    sandbox.restore();
+  })
   context('testing index', () => {
     it('should call exec with command', (done) => {
+      const execStub = sandbox.stub();
+      mock('child_process', { exec: execStub });
+      process.argv.push("--file=./test/basic.js");
+      process.env.DEBUG = 'puppeteer-loadtest';
+
+      const index = require('../bin');
+
       expect(execStub).to.have.been.called();
       expect(execStub).to.have.been.calledWith('node ./test/basic.js');
       done();
     });
+
+    it('should call exec with command and id', (done) => {
+      const execStub = sandbox.stub();
+      mock('child_process', { exec: execStub });
+      process.argv.push("--file=./test/basic.js");
+      process.argv.push("--addId");
+      process.env.DEBUG = 'puppeteer-loadtest';
+
+      const index = require('../bin');
+
+      expect(execStub).to.have.been.called();
+      expect(execStub).to.have.been.calledWith('node ./test/basic.js 0');
+      done();
+    });
   });
+
 });
-  
